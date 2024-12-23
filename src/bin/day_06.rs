20238f1a09da23560d7d6e7ae7318b321fe8b2ad
michapau <raspar_123@yahoo.fr>
2024-12-23 18:23:22 +0100

    day_06 part 2

--- src/bin/day_06.rs
+++ src/bin/day_06.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(u8)]
 pub enum Direction {
     Up,
@@ -41,7 +41,7 @@ impl Direction {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point {
     pub x: isize,
     pub y: isize,
@@ -65,6 +65,21 @@ impl Grid {
         }
     }
 
+    pub fn print_grid(&self) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                print!(
+                    "{}",
+                    self.get_value_from_point(Point {
+                        x: x as isize,
+                        y: y as isize
+                    })
+                    .unwrap()
+                )
+            }
+            print!("\n");
+        }
+    }
     pub fn get_value_from_point(&self, p: Point) -> Option<&char> {
         if p.x < 0 || p.x >= self.width as isize || p.y < 0 || p.y >= self.height as isize {
             None
@@ -98,6 +113,22 @@ impl Grid {
         }
     }
 
+    pub fn collect_possible_obstacles(&self) -> Vec<usize> {
+        let r: Vec<usize> = self
+            .array
+            .iter()
+            .enumerate()
+            .filter_map(|(i, entry)| {
+                if *entry == 'X' {
+                    //Some(self.get_pos_from_index(i))
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+            .collect();
+        r
+    }
     pub fn walk(&mut self, from: &mut Point, dir: &mut Direction) -> usize {
         let mut steps = 1;
         loop {
@@ -149,6 +180,72 @@ impl Grid {
 
         steps
     }
+    pub fn obstacle_walk(&mut self, from: &mut Point, dir: &mut Direction) -> bool {
+        let mut obstacle_bums: HashMap<(usize, Direction), usize> = HashMap::new();
+        let endless_loop;
+        loop {
+            let check_pos_at = match dir {
+                Direction::Up => Point {
+                    x: from.x,
+                    y: from.y - 1,
+                },
+                Direction::Down => Point {
+                    x: from.x,
+                    y: from.y + 1,
+                },
+                Direction::Left => Point {
+                    x: from.x - 1,
+                    y: from.y,
+                },
+                Direction::Right => Point {
+                    x: from.x + 1,
+                    y: from.y,
+                },
+            };
+
+            let value = self.get_value_from_point(check_pos_at);
+
+            match value {
+                Some('.') => {
+                    from.x = check_pos_at.x;
+                    from.y = check_pos_at.y;
+                }
+                Some('#') => {
+                    let index = self.get_index_from_pos(check_pos_at);
+                    *obstacle_bums.entry((index, *dir)).or_insert(1) += 1;
+                    if *obstacle_bums.get(&(index, *dir)).unwrap() > 2 {
+                        endless_loop = true;
+                        break;
+                    }
+
+                    dir.turn();
+                }
+                Some('X') => {
+                    from.x = check_pos_at.x;
+                    from.y = check_pos_at.y;
+                }
+                Some('O') => {
+                    let index = self.get_index_from_pos(check_pos_at);
+                    *obstacle_bums.entry((index, *dir)).or_insert(1) += 1;
+                    if *obstacle_bums.get(&(index, *dir)).unwrap() > 2 {
+                        endless_loop = true;
+                        break;
+                    }
+                    dir.turn();
+                }
+                None => {
+                    endless_loop = false;
+                    break;
+                }
+                Some(x) => {
+                    println!("what: {}", x);
+                    panic!("walking delirium..")
+                }
+            }
+        }
+
+        endless_loop
+    }
 }
 fn main() {
     //let input = fs::read_to_string("./data/input_test_6_1.txt").unwrap();
@@ -166,12 +263,47 @@ fn main() {
     let mut g = Grid::new(w, h, data);
 
     if let Some(mut walker_pos) = g.get_walker_pos() {
-        let mut dir = Direction::try_from(*g.get_value_from_point(walker_pos).unwrap()).unwrap();
+        let init_walker_pos = Point {
+            x: walker_pos.x,
+            y: walker_pos.y,
+        };
+        let init_dir = Direction::try_from(*g.get_value_from_point(walker_pos).unwrap()).unwrap();
+        let mut dir = init_dir.clone();
         let walker_index = g.get_index_from_pos(walker_pos);
         g.array[walker_index] = 'X';
         let steps = g.walk(&mut walker_pos, &mut dir);
-        println!("walker steps: {}", steps);
+
         //right answer: 4758
+        println!("walker steps: {}", steps);
+
+        g.array[walker_index] = '.';
+
+        //part 2
+        let pos_obstacles = g.collect_possible_obstacles();
+
+        let count = pos_obstacles
+            .iter()
+            .filter_map(|&i| {
+                let mut new_walker = Point {
+                    x: init_walker_pos.x,
+                    y: init_walker_pos.y,
+                };
+                let mut dir = init_dir.clone();
+                g.array[i] = 'O';
+
+                let w_r = g.obstacle_walk(&mut new_walker, &mut dir);
+
+                g.array[i] = 'X';
+                if w_r {
+                    Some(w_r)
+                } else {
+                    None
+                }
+            })
+            .count();
+
+        //right answer: 1670
+        println!("possible obstacles count: {}", count);
     } else {
         println!("walker not found");
     }
